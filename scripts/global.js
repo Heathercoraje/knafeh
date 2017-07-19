db9cc3a70f7392f49f2161bf743c85fcf8e802da
matthewdking <matthewdking@outlook.com>
2017-07-19 16:59:28 +0300

    add tests for flag url function relates #17

--- scripts/global.js
+++ scripts/global.js
@@ -2,8 +2,9 @@
 var newsUrl = "https://content.guardianapis.com/search?q="; //the main url
 var newsUrl2 = "&api-key=7b8fdba3-2d40-4d8e-b725-99d86676fbe9&show-fields=all";
 var newsUrlIsrael = newsUrl + 'israel' + newsUrl2;
-var picUrl = "https://countryapi.gear.host/v1/Country/getCountries?pName=";
-var israelUrl = "israel";
-var picUrlIsrael = picUrl + israelUrl;
-var koreaUrl = "Korea+(Republic+of)";
-var ukUrl = "united+kingdom";
+var picUrlIsrael = "https://countryapi.gear.host/v1/Country/getCountries?pName=israel";
+// var picUrl = "https://countryapi.gear.host/v1/Country/getCountries?pName=";
+// var israelUrl = "israel";
+// var picUrlIsrael = picUrl + israelUrl;
+// var koreaUrl = "Korea+(Republic+of)";
+// var ukUrl = "united+kingdom";
--- tests/test.js
+++ tests/test.js
@@ -1,8 +1,23 @@
 var test = require('tape');
-var fetch = require('./updateContent.js');
+var fetch = require('../scripts/updateContent.js');
+
 
 
 test('check tape is working', function(t) {
   t.equal(1, 1, 'one should equal one');
+  t.end();
+});
+
+test('check flagUrl returns UK', function(t) {
+  var expected = "https://countryapi.gear.host/v1/Country/getCountries?pName=united+kingdom";
+  var result = fetch('UK')
+  t.equal(result, expected, 'Expected url for UK flag');
+  t.end();
+});
+
+test('check flagUrl returns South Korea', function(t) {
+  var expected = "https://countryapi.gear.host/v1/Country/getCountries?pName=Korea+(Republic+of)";
+  var result = fetch('South Korea')
+  t.equal(result, expected, 'Expected url for South Korea flag');
   t.end();
 });
